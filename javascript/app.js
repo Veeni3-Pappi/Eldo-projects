cd9df89d66f39fc09d7e18d1777def90abceb5c2
Veeni3-Pappi <Vincenthiuhu@proton.me>
2026-07-07 11:37:51 +0300

    data types and variables

---
+++ javascript/app.js
@@ -0,0 +1,38 @@
+// variables- storing data in memory
+
+// in js , we can store  data in variables using the var, let or const keywords 
+ // keywords   are words that have special meaning in a programming language.
+
+ // var and let are used for data that can change over time while const i used for data that doesnt chnage
+
+ var userName =" casual ace"
+ let email= "typeshi@gmail.com"
+ let isKenyan = true 
+ const PI = 3.14159
+
+
+ let users = ["john", "jane", "joe", "smith"]
+
+ let product={
+    name: "laptop",
+    price: 1000,
+    inStock: true
+}
+
+// data types - the type of data that can be stored in a variable
+
+// in js- we have v different data types that can be stored in variables. These include string, number,, boolean, object, null and undefined
+
+// operators used to perform operations on variables and values
+// arithmetic operators +,-,*,/
+
+//compsarison operators ==, ===, !=, !==, >, <, >=, <=
+
+//logical operators &&, ||, !
+
+// assignment operators =, +=, -=, *=, /=
+// task - explain variable naming conventions in js.
+// task2 
+// differenciate between == and ===
+//task 3 ... explain logical operators and how they are used in js 
+

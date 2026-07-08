01bbb66f25784675dfac80f2ecbe452ad3980b81
Veeni3-Pappi <Vincenthiuhu@proton.me>
2026-07-08 11:24:59 +0300

    &&, ||, ! operators

---
+++ javascript/logic.js
@@ -0,0 +1,21 @@
+// logical operatorsare used to combine two or more conditions. they are often used with conditional statements  to control the flow of a program based on certain conditions.
+
+
+const age = 83
+const height = 182
+
+ console .log(age==80)
+
+ if(age>80 || height>180){
+    console.log("this is a well exercised peron")}
+
+// && and (Operator)
+// || or 
+// ! not     !true = false
+
+
+if(!(age < 40)){
+    console.log("Mature adult")
+}
+
+    // explore the truth table

f4e47420cd3d10eb1ffa9f335a65acd51d74a1d2
Veeni3-Pappi <Vincenthiuhu@proton.me>
2026-07-14 17:40:07 +0300

    Fix fizz buzz typo: change fizzbbuzz to fizzbuzz

--- javascript/for_loops.js
+++ javascript/for_loops.js
@@ -4,7 +4,7 @@
 for (let x=1; x <=100; x++ ){
 
     if (x % 3  === 0 && x % 5 === 0 ){
-        console.log("fizzbbuzz");
+        console.log("fizzbuzz");
     }
     else if (x % 3=== 0){
         console.log("fizz"); 
